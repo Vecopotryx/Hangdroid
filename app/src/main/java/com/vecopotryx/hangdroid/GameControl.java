3aa39a2307ce90d3b2606c36fccb84f27251ad39
Vecopotryx <Repsejk@gmail.com>
2020-05-14 09:25:23 +0200

    Testing input

---
+++ app/src/main/java/com/vecopotryx/hangdroid/GameControl.java
@@ -0,0 +1,7 @@
+package com.vecopotryx.hangdroid;
+
+public class GameControl {
+    public static void start(){
+        Model.set_answer("test");
+    }
+}
--- app/src/main/java/com/vecopotryx/hangdroid/MainActivity.java
+++ app/src/main/java/com/vecopotryx/hangdroid/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,10 +15,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        GameControl.start();
     }
 
+    String input;
     public void method(View v){
+        // Button mButton;
+        EditText mEdit;
+        TextView mText;
+
+        mEdit   = (EditText)findViewById(R.id.editText);
+        Model.set_answer(mEdit.getText().toString());
+
         TextView tv = (TextView)findViewById(R.id.textView);
-        tv.setText("Test text");
+        tv.setText(Model.get_answer());
     }
 }
