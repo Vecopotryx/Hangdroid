1d2e224360c640dc81b3ad14d572fec9b393e1fd
Vecopotryx <Repsejk@gmail.com>
2020-05-15 11:25:29 +0200

    Working on adding dark mode

--- app/src/main/java/com/vecopotryx/hangdroid/MainActivity.java
+++ app/src/main/java/com/vecopotryx/hangdroid/MainActivity.java
@@ -1,11 +1,15 @@
 package com.vecopotryx.hangdroid;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
             handleExit();
         }
 
+        
         Button randomGameButton = (Button)findViewById(R.id.randomGameButton);
         randomGameButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,6 +47,9 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 // do stuff
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                finish();
+                startActivity(getIntent());
             }
         });
 
@@ -50,6 +58,38 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 handleExit();
+            }
+        });
+
+        final Switch darkModeSwitch = (Switch)findViewById((R.id.darkModeSwitch));
+
+        CheckBox toggleAutoMode = (CheckBox)findViewById(R.id.toggleAutoMode);
+        toggleAutoMode.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener()
+        {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked)
+            {
+                if(isChecked)
+                {
+                    darkModeSwitch.setClickable(false);
+                } else {
+                    darkModeSwitch.setClickable(true);
+                }
+
+            }
+        });
+
+        darkModeSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                    finish();
+                    startActivity(getIntent());
+                } else {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                    finish();
+                    startActivity(getIntent());
+                }
             }
         });
     }
